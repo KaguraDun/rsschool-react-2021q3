d55fce866e6e152f7d32ad9f9542e4d0ad587120
Vasily Kovnev <v.k95@yandex.ru>
2021-08-02 20:53:11 +0300

    feat: add card list on page

--- react-components/src/index.jsx
+++ react-components/src/index.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './styles/index.scss';
 import SearchBar from './components/SearchBar/SearchBar';
+import CardList from './components/CardList/CardList';
+import cardData from './model/cardData';
 
 ReactDOM.render(
   <div className={style.container}>
     <div className={style.SearchBarWrapper}>
       <SearchBar />
+    </div>
+    <div className={style.cardsWrapper}>
+      <CardList cardData={cardData} />
     </div>
   </div>,
   document.getElementById('root')

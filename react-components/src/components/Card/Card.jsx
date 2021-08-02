74cfae67e9106036851f5cc86c28530d883f0049
Vasily Kovnev <v.k95@yandex.ru>
2021-08-02 20:39:08 +0300

    feat: add card component

---
+++ react-components/src/components/Card/Card.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import style from './Card.scss';
+import Like from '../Like/Like';
+
+const Card = ({ cardData }) => (
+  <article className={style.card}>
+    <img alt="" className={style.thumbnail} src={cardData.thumbnail} />
+    <div className={style.cardContent}>
+      <header>
+        <a className={style.cardLink} href={cardData.link}>
+          <h3 className={style.title}>{cardData.title}</h3>
+        </a>
+
+        <p className={style.author}>by {cardData.author}</p>
+      </header>
+      <p className={style.description}>{cardData.description}</p>
+      <footer className={style.footer}>
+        <Like likesNumber={cardData.likesNumber} />
+        <p className={style.date}>{cardData.date}</p>
+      </footer>
+    </div>
+  </article>
+);
+
+export default Card;

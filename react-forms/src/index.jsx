590e9fd3f8c3ff21fadc22e6c0c18b9fbfb1301b
Vasily Kovnev <v.k95@yandex.ru>
2021-08-06 19:45:44 +0300

    feat: add form component to page

--- react-forms/src/index.jsx
+++ react-forms/src/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import CardForm from './components/CardForm/CardForm';
 import style from './styles/index.scss';
 
 ReactDOM.render(
-  <div className={style.container} />,
+  <div className={style.container}>
+    <CardForm />
+  </div>,
   document.getElementById('root')
 );

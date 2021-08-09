4b1a877b9a8af1c72e7658153c73f494aeb0e007
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2021-08-09 15:19:12 +0300

    feat: add food list and toppings list as separate data

---
+++ react-forms/src/model/foodList.js
@@ -0,0 +1,7 @@
+const foodList = {
+  0: { value: 'chicken', name: 'С курицей' },
+  1: { value: 'pork', name: 'Со свининой' },
+  2: { value: 'vegan', name: 'С овощами' },
+};
+
+export default foodList;
---
+++ react-forms/src/model/toppingsList.js
@@ -0,0 +1,13 @@
+const toppingsList = {
+  0: {
+    name: 'Сыр',
+  },
+  1: {
+    name: 'Картофель фри',
+  },
+  2: {
+    name: 'Сырный лаваш',
+  },
+};
+
+export default toppingsList;

c034dfdf1d33d6e65d36578ce45c6a4e1f64c837
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2021-08-09 23:25:34 +0300

    feat: add success message after form submit

--- react-forms/src/components/App/App.jsx
+++ react-forms/src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import foodList from '../../model/foodList';
 import toppingsList from '../../model/toppingsList';
@@ -8,10 +8,19 @@ import style from './App.scss';
 
 const App = () => {
   const [cardItems, setCardItems] = useState([]);
+  const [showMessage, setShowMessage] = useState(false);
 
   const createCard = (formData) => {
     setCardItems([formData, ...cardItems]);
+    setShowMessage(true);
   };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setShowMessage(false);
+    }, 2000);
+    return () => clearInterval(timer);
+  });
 
   return (
     <div className={style.container}>
@@ -20,7 +29,14 @@ const App = () => {
         foodOptions={foodList}
         toppingsOptions={toppingsList}
       />
-      <CardList items={cardItems} />
+      <div className={style.wrapper}>
+        <div className={style.messageWrapper}>
+          <p className={style.message}>
+            {showMessage ? 'Данные сохранены' : false}
+          </p>
+        </div>
+        <CardList items={cardItems} />
+      </div>
     </div>
   );
 };

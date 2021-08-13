8a1eab4ebc965f03f91f57f8de3b2b5171cfc5c1
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2021-08-14 00:03:13 +0300

    feat: add search options component

---
+++ react-api/src/components/SearchOptions/SearchOptions.jsx
@@ -0,0 +1,73 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+import style from './SearchOptions.scss';
+
+const SearchOptions = ({ handleOptionChange, options, maxPages }) => (
+  <div className={style.options}>
+    <label className={style.label}>
+      Sort order
+      <select
+        className={style.select}
+        onChange={(e) => handleOptionChange('sort', e.target.value)}
+        value={options.sort}
+      >
+        <option value="relevance">relevance</option>
+        <option value="date-posted-asc">date-posted-asc</option>
+        <option value="date-posted-desc">date-posted-desc</option>
+      </select>
+    </label>
+    <label className={style.label}>
+      Results per page (1-50)
+      <input
+        className={style.input}
+        max="50"
+        min="1"
+        onChange={(e) => handleOptionChange('resultsPerPage', e.target.value)}
+        type="number"
+        value={options.resultsPerPage}
+      />
+    </label>
+    <button
+      className={style.button}
+      onClick={() =>
+        handleOptionChange(
+          'currentPage',
+          options.currentPage - 1 > 0
+            ? options.currentPage - 1
+            : options.currentPage
+        )
+      }
+      type="button"
+    >
+      {'<'}
+    </button>
+    <label className={style.label}>
+      Cur page {}
+      <input
+        className={style.input}
+        max={maxPages}
+        min="1"
+        onChange={(e) => handleOptionChange('currentPage', e.target.value)}
+        type="number"
+        value={options.currentPage}
+      />
+    </label>
+    <button
+      className={style.button}
+      onClick={() =>
+        handleOptionChange(
+          'currentPage',
+          options.currentPage + 1 <= maxPages
+            ? options.currentPage + 1
+            : options.currentPage
+        )
+      }
+      type="button"
+    >
+      {'>'}
+    </button>
+  </div>
+);
+
+export default SearchOptions;

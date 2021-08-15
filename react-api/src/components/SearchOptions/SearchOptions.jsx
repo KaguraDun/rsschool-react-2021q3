3557aebf86c62273af8ad930f67317b7650c04a9
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2021-08-15 14:45:40 +0300

    fix: pagination bug when next page button adds wrong value
    refactor: move function inside button on click  in separate function

--- react-api/src/components/SearchOptions/SearchOptions.jsx
+++ react-api/src/components/SearchOptions/SearchOptions.jsx
@@ -3,73 +3,70 @@ import React from 'react';
 
 import style from './SearchOptions.scss';
 
-const SearchOptions = ({ handleOptionChange, options, maxPages }) => (
-  <div className={style.options}>
-    <label className={style.label}>
-      Sort order
-      <select
-        className={style.select}
-        onChange={(e) => handleOptionChange('sort', e.target.value)}
-        value={options.sort}
-      >
-        <option value="relevance">relevance</option>
-        <option value="date-posted-asc">date-posted-asc</option>
-        <option value="date-posted-desc">date-posted-desc</option>
-      </select>
-    </label>
-    <label className={style.label}>
-      Results per page (1-50)
-      <input
-        className={style.input}
-        max="50"
-        min="1"
-        onChange={(e) => handleOptionChange('resultsPerPage', e.target.value)}
-        type="number"
-        value={options.resultsPerPage}
-      />
-    </label>
-    <div className={style.pagination}>
-      <button
-        className={style.button}
-        onClick={() =>
-          handleOptionChange(
-            'currentPage',
-            options.currentPage - 1 > 0
-              ? options.currentPage - 1
-              : options.currentPage
-          )
-        }
-        type="button"
-      >
-        {'<'}
-      </button>
+const SearchOptions = ({ handleOptionChange, options, maxPages }) => {
+  const getPage = (direction) => {
+    const directions = { next: 1, prev: -1 };
+    const pageNumber = Number(options.currentPage) + directions[direction];
+
+    return pageNumber > 0 && pageNumber < maxPages
+      ? pageNumber
+      : options.currentPage;
+  };
+
+  return (
+    <div className={style.options}>
       <label className={style.label}>
-        Page/max:{maxPages || '?'}
+        Sort order
+        <select
+          className={style.select}
+          onChange={(e) => handleOptionChange('sort', e.target.value)}
+          value={options.sort}
+        >
+          <option value="relevance">relevance</option>
+          <option value="date-posted-asc">date-posted-asc</option>
+          <option value="date-posted-desc">date-posted-desc</option>
+        </select>
+      </label>
+      <label className={style.label}>
+        Results per page (1-50)
         <input
           className={style.input}
-          max={maxPages}
+          max="50"
           min="1"
-          onChange={(e) => handleOptionChange('currentPage', e.target.value)}
+          onChange={(e) => handleOptionChange('resultsPerPage', e.target.value)}
           type="number"
-          value={options.currentPage}
+          value={options.resultsPerPage}
         />
       </label>
-      <button
-        className={style.button}
-        onClick={() =>
-          handleOptionChange(
-            'currentPage',
-            options.currentPage + 1 <= maxPages
-              ? options.currentPage + 1
-              : options.currentPage
-          )
-        }
-        type="button"
-      >
-        {'>'}
-      </button>
+      <div className={style.pagination}>
+        <button
+          className={style.button}
+          onClick={() => handleOptionChange('currentPage', getPage('prev'))}
+          type="button"
+        >
+          {'<'}
+        </button>
+        <label className={style.label}>
+          Page/max:{maxPages || '?'}
+          <input
+            className={style.input}
+            max={maxPages}
+            min="1"
+            onChange={(e) => handleOptionChange('currentPage', e.target.value)}
+            type="number"
+            value={options.currentPage}
+          />
+        </label>
+        <button
+          className={style.button}
+          onClick={() => handleOptionChange('currentPage', getPage('next'))}
+          type="button"
+        >
+          {'>'}
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default SearchOptions;

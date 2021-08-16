a32e2454268d591e8e20e41d2f4d98e804ae1b94
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2021-08-16 22:36:48 +0300

    feat: add details component

---
+++ react-routing/src/components/Details/Details.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from 'react';
+
+import Preloader from '../Preloader/Preloader';
+import style from './Details.scss';
+
+const Details = ({ itemId, apiService }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [info, setInfo] = useState(null);
+
+  useEffect(() => {
+    apiService
+      .getInfo(itemId)
+      .then((data) => {
+        setInfo(data.photo);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setIsLoading(false);
+        setIsError(true);
+      });
+  }, [apiService, itemId]);
+
+  const InfoInJson = () => (
+    <code className={style.info}>
+      <pre>{JSON.stringify(info, null, 4)}</pre>
+    </code>
+  );
+
+  return (
+    <div className={style.details}>
+      {isLoading ? <Preloader /> : null}
+      {isError ? 'Error occurred' : null}
+      {info ? <InfoInJson /> : null}
+    </div>
+  );
+};
+
+export default Details;
